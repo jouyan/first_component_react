bd9823fddcc87b65b310bfdbae63c771c73c19b3
Jouyan <122379963+jouyan@users.noreply.github.com>
2024-04-10 23:53:51 +0100

    add new  section

---
+++ react-components/src/App2.js
@@ -0,0 +1,30 @@
+import './App.css';
+function App() {
+  return (
+    <div>
+       <User />
+       <Job jobTitle='Boss' age={45} salary={550}  />
+    </div>
+  );
+}
+const User=()=>{
+  return(
+    <div>
+    <h1>Second (2th) Part:</h1>
+    <h2>Details:</h2>
+    </div>
+  );
+}
+
+const Job=(props)=>{
+  return(
+    <div>
+    <h1>{props.jobTitle}</h1>
+    <h1>{props.age}</h1>
+    <h1>{props.salary}</h1>
+    </div>
+
+
+  );
+}
+export default App;
