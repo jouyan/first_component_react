f9655c1f89d14fda9fea8a1c21294319b7a56e6e
Jouyan <122379963+jouyan@users.noreply.github.com>
2024-04-15 21:55:31 +0100

    test edit

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -1,30 +1,16 @@
-import './App.css';
+import Line from './Line';
+
 function App() {
   return (
     <div>
-       <User />
-       <Job jobTitle='Boss' age={45} salary={550}  />
-    </div>
-  );
-}
-const User=()=>{
-  return(
-    <div>
-    <h1>first react App</h1>
-    <h3>Members</h3>
-    </div>
-  );
-}
+       <h1>start</h1>
+       <Line />
+       <Line />
+       <Line />
+       <h3>finish</h3>
 
-const Job=(props)=>{
-  return(
-    <div>
-      <p>{props.jobTitle}</p>
-      <p>{props.age}</p>
-      <p>{props.salary}</p>
+       
     </div>
-
-
   );
 }
 export default App;
--- react-components/src/Line.js
+++ react-components/src/Line.js
@@ -2,28 +2,13 @@
 function Line() {
   return (
     <div>
-       <User />
+      <button>Click to line</button>
+      <hr />
       
     </div>
   );
 }
-const User=()=>{
-  return(
-    <div>
-    <hr />
-    </div>
-  );
-}
-
-// const Job=(props)=>{
-//   return(
-//     <div>
-//     <h1>{props.jobTitle}</h1>
-//     <h1>{props.age}</h1>
-//     <h1>{props.salary}</h1>
-//     </div>
 
 
-//   );
-// }
+
 export default Line;
